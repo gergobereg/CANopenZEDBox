df4c2977d3c048d6677204f82286dfc9e0d269ef
gergobereg <gergobereg@users.noreply.github.com>
2026-03-17 10:13:13 +0100

    ZEDBoxMini_V7.eds RPDO 1 using TPDO 1 using + 1int with terminal input PDO3800=<value>

--- CO_application.c
+++ CO_application.c
@@ -2,13 +2,60 @@
 #include "OD.h"
 #include "301/CO_PDO.h"
 
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void app_processTerminalCommand(CO_t* co, const char* line) {
+    char* end = NULL;
+    long value = 0;
+
+    if (strncmp(line, "PDO3800=", 8) == 0) {
+        errno = 0;
+        value = strtol(&line[8], &end, 0);
+        if (errno == 0 && end != &line[8] && *end == '\0' && value >= 0 && value <= 255) {
+            CO_LOCK_OD(co->CANmodule);
+            OD_RAM.x3800_digital_Inputs1[0] = (uint8_t)value;
+            CO_UNLOCK_OD(co->CANmodule);
+            CO_TPDOsendRequest(&co->TPDO[0]);
+            printf("TPDO 0x3800:01 set to %ld\n", value);
+        } else {
+            printf("Invalid PDO3800 value. Use 0..255.\n");
+        }
+        fflush(stdout);
+    } else if (strncmp(line, "PDO3801=", 8) == 0) {
+        errno = 0;
+        value = strtol(&line[8], &end, 0);
+        if (errno == 0 && end != &line[8] && *end == '\0' && value >= -32768 && value <= 32767) {
+            CO_LOCK_OD(co->CANmodule);
+            OD_RAM.x3801_digital_Inputs2[0] = (int16_t)value;
+            CO_UNLOCK_OD(co->CANmodule);
+            CO_TPDOsendRequest(&co->TPDO[1]);
+            printf("TPDO 0x3801:01 set to %ld\n", value);
+        } else {
+            printf("Invalid PDO3801 value. Use -32768..32767.\n");
+        }
+        fflush(stdout);
+    } else if (line[0] != '\0') {
+        printf("Unknown command. Use PDO3800=<0..255> or PDO3801=<-32768..32767>.\n");
+        fflush(stdout);
+    }
+}
 
 CO_ReturnError_t app_programStart(uint16_t* bitRate, uint8_t* nodeId, uint32_t* errInfo) {
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+
     (void)bitRate;
     (void)nodeId;
     (void)errInfo;
+
+    if (flags >= 0) {
+        (void)fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+    }
+
     return CO_ERROR_NO;
 }
 
@@ -21,50 +68,57 @@ void app_programEnd() {}
 
 // USER APPLICATION
 void app_programAsync(CO_t* co, uint32_t timer1usDiff) {
+    static char cmdBuf[128];
+    static size_t cmdLen = 0;
+    char readBuf[64];
+    ssize_t bytesRead = 0;
+
     (void)timer1usDiff;
 
     /* PLC -> Linux (RPDO mapped to 0x3000:01) */
     uint8_t plc_out;
-    int16_t plc_out_int;
     CO_LOCK_OD(co->CANmodule);
     plc_out = OD_RAM.x3000_digital_Outputs1[0];
-    plc_out_int = OD_RAM.x3001_digital_Outputs2[0];
     CO_UNLOCK_OD(co->CANmodule);
 
     /* Linux -> PLC (TPDO mapped to 0x3800:01 and 0x3801:01) */
     static uint8_t last_in = 0;
     static uint8_t last_out = 0;
-    static int16_t last_in_int = 0;
-    static int16_t last_out_int = 0;
 
     if (plc_out != last_out) {
         printf("RPDO 0x3000:01 changed: 0x%02X\n", plc_out);
         fflush(stdout);
         last_out = plc_out;
     }
-    if (plc_out != last_in) {
-        CO_LOCK_OD(co->CANmodule);
-        OD_RAM.x3800_digital_Inputs1[0] = plc_out;
-        CO_UNLOCK_OD(co->CANmodule);
 
-        /* Request TPDO send */
-        CO_TPDOsendRequest(&co->TPDO[0]);
-        last_in = plc_out;
+    (void)last_in;
+
+    while ((bytesRead = read(STDIN_FILENO, readBuf, sizeof(readBuf))) > 0) {
+        for (ssize_t i = 0; i < bytesRead; i++) {
+            char ch = readBuf[i];
+
+            if (ch == '\r') {
+                continue;
+            }
+            if (ch == '\n') {
+                cmdBuf[cmdLen] = '\0';
+                app_processTerminalCommand(co, cmdBuf);
+                cmdLen = 0;
+                continue;
+            }
+            if (cmdLen < (sizeof(cmdBuf) - 1U)) {
+                cmdBuf[cmdLen++] = ch;
+            } else {
+                printf("Command too long.\n");
+                fflush(stdout);
+                cmdLen = 0;
+            }
+        }
     }
 
-    if (plc_out_int != last_out_int) {
-        printf("RPDO 0x3001:01 changed: %" PRId16 " (0x%04X)\n", plc_out_int, (uint16_t)plc_out_int);
+    if (bytesRead < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+        printf("Terminal read error.\n");
         fflush(stdout);
-        last_out_int = plc_out_int;
-    }
-    if (plc_out_int != last_in_int) {
-        CO_LOCK_OD(co->CANmodule);
-        OD_RAM.x3801_digital_Inputs2[0] = plc_out_int;
-        CO_UNLOCK_OD(co->CANmodule);
-
-        /* Request TPDO send */
-        CO_TPDOsendRequest(&co->TPDO[1]);
-        last_in_int = plc_out_int;
     }
 }
 
--- myDevice/OD.c
+++ myDevice/OD.c
@@ -48,18 +48,9 @@ OD_ATTR_RAM OD_RAM_t OD_RAM = {
         .COB_IDUsedByRPDO = 0x00000200,
         .transmissionType = 0xFF
     },
-    .x1401_RPDOCommunicationParameter = {
-        .highestSub_indexSupported = 0x02,
-        .COB_IDUsedByRPDO = 0x00000300,
-        .transmissionType = 0xFF
-    },
     .x1600_RPDOMappingParameter = {
         .numberOfMappedApplicationObjectsInPDO = 0x01,
         .mappingEntry = 0x30000108
-    },
-    .x1601_RPDOMappingParameter = {
-        .numberOfMappedApplicationObjectsInPDO = 0x01,
-        .mappingEntry = 0x30010110
     },
     .x1800_TPDOCommunicationParameter = {
         .highestSub_indexSupported = 0x05,
@@ -87,8 +78,6 @@ OD_ATTR_RAM OD_RAM_t OD_RAM = {
     },
     .x3000_digital_Outputs1_sub0 = 0x01,
     .x3000_digital_Outputs1 = {0x00},
-    .x3001_digital_Outputs2_sub0 = 0x01,
-    .x3001_digital_Outputs2 = {0x0000},
     .x3800_digital_Inputs1_sub0 = 0x01,
     .x3800_digital_Inputs1 = {0x00},
     .x3801_digital_Inputs2_sub0 = 0x01,
@@ -114,15 +103,12 @@ typedef struct {
     OD_obj_record_t o_1018_identityObject[4];
     OD_obj_record_t o_1200_serverSdoParameter[3];
     OD_obj_record_t o_1400_RPDOCommunicationParameter[3];
-    OD_obj_record_t o_1401_RPDOCommunicationParameter[3];
     OD_obj_record_t o_1600_RPDOMappingParameter[2];
-    OD_obj_record_t o_1601_RPDOMappingParameter[2];
     OD_obj_record_t o_1800_TPDOCommunicationParameter[6];
     OD_obj_record_t o_1801_TPDOCommunicationParameter[6];
     OD_obj_record_t o_1A00_TPDOMappingParameter[2];
     OD_obj_record_t o_1A01_TPDOMappingParameter[2];
     OD_obj_array_t o_3000_digital_Outputs1;
-    OD_obj_array_t o_3001_digital_Outputs2;
     OD_obj_array_t o_3800_digital_Inputs1;
     OD_obj_array_t o_3801_digital_Inputs2;
 } ODObjs_t;
@@ -250,26 +236,6 @@ static CO_PROGMEM ODObjs_t ODObjs = {
             .dataLength = 1
         }
     },
-    .o_1401_RPDOCommunicationParameter = {
-        {
-            .dataOrig = &OD_RAM.x1401_RPDOCommunicationParameter.highestSub_indexSupported,
-            .subIndex = 0,
-            .attribute = ODA_SDO_R,
-            .dataLength = 1
-        },
-        {
-            .dataOrig = &OD_RAM.x1401_RPDOCommunicationParameter.COB_IDUsedByRPDO,
-            .subIndex = 1,
-            .attribute = ODA_SDO_RW | ODA_MB,
-            .dataLength = 4
-        },
-        {
-            .dataOrig = &OD_RAM.x1401_RPDOCommunicationParameter.transmissionType,
-            .subIndex = 2,
-            .attribute = ODA_SDO_RW,
-            .dataLength = 1
-        }
-    },
     .o_1600_RPDOMappingParameter = {
         {
             .dataOrig = &OD_RAM.x1600_RPDOMappingParameter.numberOfMappedApplicationObjectsInPDO,
@@ -279,20 +245,6 @@ static CO_PROGMEM ODObjs_t ODObjs = {
         },
         {
             .dataOrig = &OD_RAM.x1600_RPDOMappingParameter.mappingEntry,
-            .subIndex = 1,
-            .attribute = ODA_SDO_R | ODA_MB,
-            .dataLength = 4
-        }
-    },
-    .o_1601_RPDOMappingParameter = {
-        {
-            .dataOrig = &OD_RAM.x1601_RPDOMappingParameter.numberOfMappedApplicationObjectsInPDO,
-            .subIndex = 0,
-            .attribute = ODA_SDO_R,
-            .dataLength = 1
-        },
-        {
-            .dataOrig = &OD_RAM.x1601_RPDOMappingParameter.mappingEntry,
             .subIndex = 1,
             .attribute = ODA_SDO_R | ODA_MB,
             .dataLength = 4
@@ -410,14 +362,6 @@ static CO_PROGMEM ODObjs_t ODObjs = {
         .dataElementLength = 1,
         .dataElementSizeof = sizeof(uint8_t)
     },
-    .o_3001_digital_Outputs2 = {
-        .dataOrig0 = &OD_RAM.x3001_digital_Outputs2_sub0,
-        .dataOrig = &OD_RAM.x3001_digital_Outputs2[0],
-        .attribute0 = ODA_SDO_R,
-        .attribute = ODA_SDO_RW | ODA_RPDO,
-        .dataElementLength = 2,
-        .dataElementSizeof = sizeof(int16_t)
-    },
     .o_3800_digital_Inputs1 = {
         .dataOrig0 = &OD_RAM.x3800_digital_Inputs1_sub0,
         .dataOrig = &OD_RAM.x3800_digital_Inputs1[0],
@@ -454,15 +398,12 @@ static OD_ATTR_OD OD_entry_t ODList[] = {
     {0x1018, 0x04, ODT_REC, &ODObjs.o_1018_identityObject, NULL},
     {0x1200, 0x03, ODT_REC, &ODObjs.o_1200_serverSdoParameter, NULL},
     {0x1400, 0x03, ODT_REC, &ODObjs.o_1400_RPDOCommunicationParameter, NULL},
-    {0x1401, 0x03, ODT_REC, &ODObjs.o_1401_RPDOCommunicationParameter, NULL},
     {0x1600, 0x02, ODT_REC, &ODObjs.o_1600_RPDOMappingParameter, NULL},
-    {0x1601, 0x02, ODT_REC, &ODObjs.o_1601_RPDOMappingParameter, NULL},
     {0x1800, 0x06, ODT_REC, &ODObjs.o_1800_TPDOCommunicationParameter, NULL},
     {0x1801, 0x06, ODT_REC, &ODObjs.o_1801_TPDOCommunicationParameter, NULL},
     {0x1A00, 0x02, ODT_REC, &ODObjs.o_1A00_TPDOMappingParameter, NULL},
     {0x1A01, 0x02, ODT_REC, &ODObjs.o_1A01_TPDOMappingParameter, NULL},
     {0x3000, 0x02, ODT_ARR, &ODObjs.o_3000_digital_Outputs1, NULL},
-    {0x3001, 0x02, ODT_ARR, &ODObjs.o_3001_digital_Outputs2, NULL},
     {0x3800, 0x02, ODT_ARR, &ODObjs.o_3800_digital_Inputs1, NULL},
     {0x3801, 0x02, ODT_ARR, &ODObjs.o_3801_digital_Inputs2, NULL},
     {0x0000, 0x00, 0, NULL, NULL}
